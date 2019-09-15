60a9cd8cacd842d55158e595285426b072983fa8
Kevin Li <kevinli29033@gmail.com>
2019-09-15 13:08:39 -0400

    Initial Commit

---
+++ PingPongWinTracker/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PingPongWinTracker
+//
+//  Created by Kevin Li on 9/15/19.
+//  Copyright © 2019 Kevin Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PingPongWinTracker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PingPongWinTracker
+//
+//  Created by Kevin Li on 9/15/19.
+//  Copyright © 2019 Kevin Li. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
